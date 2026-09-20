52091fccab4bcf541bf3fcae10e1354f78b33946
Daniel (Haven) Hotyanov <49914607+danielh-official@users.noreply.github.com>
2026-09-20 09:16:54 -0400

    Refactor HTMLNode constructor for improved readability and fix __repr__ method to return a string representation

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,6 +1,10 @@
 class HTMLNode:
     def __init__(
-        self, tag: str | None = None, value: str | None = None, children: list["HTMLNode"] | None = None, props: dict[str, str] | None = None
+        self, 
+        tag: str | None = None, 
+        value: str | None = None,
+        children: list["HTMLNode"] | None = None,
+        props: dict[str, str] | None = None
     ):
         self.tag = tag
         self.value = value
@@ -21,7 +25,6 @@ class HTMLNode:
     def props_to_html(self) -> str:
         return "".join(f' {key}="{value}"' for key, value in self.props.items())
     
-    def __repr__(self) -> None:
-        print(self)
-        
+    def __repr__(self) -> str:
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
     
